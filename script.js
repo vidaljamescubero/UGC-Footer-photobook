1837a2f06580f60f48cf1ea24befd2c92da64b45
vidal james cubero <cubero.vj@gmail.com>
2022-06-24 21:27:13 +0800

    first commit

---
+++ script.js
@@ -0,0 +1,36 @@
+window.ugcFooterContainer = document.querySelector(".ugc-footer-container");
+window.ugcFooter = document.querySelector('.ugc-footer')
+
+window.addEventListener('resize', () => {
+    if (ugcFooterContainer.offsetWidth !== ugcFooter.offsetWidth) {
+        let diff = ugcFooterContainer.offsetWidth / ugcFooter.offsetWidth;
+        let height = ugcFooter.offsetHeight;
+        let heightValue = `${diff * height}px`
+        console.log(heightValue)
+
+        ugcFooter.style.cssText = `
+            transform: scale(${diff});
+            transform-origin: top left;
+        `
+
+        ugcFooterContainer.style.cssText = `
+            height: ${heightValue};
+        `
+    }
+})
+
+if (ugcFooterContainer.offsetWidth !== ugcFooter.offsetWidth) {
+    let diff = ugcFooterContainer.offsetWidth / ugcFooter.offsetWidth;
+    let height = ugcFooter.offsetHeight;
+    let heightValue = `${diff * height}px`
+    console.log(heightValue)
+
+    ugcFooter.style.cssText = `
+        transform: scale(${diff});
+        transform-origin: top left;
+    `
+
+    ugcFooterContainer.style.cssText = `
+        height: ${heightValue};
+    `
+}
